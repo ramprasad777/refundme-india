7178fa529af32baf7d1c8ae7b7b1fd5e2adaa9cd
unknown <rpaneja@gmail.com>
2018-10-25 18:25:55 +0530

    contact page update

--- refundme-india/src/app/components/contact-us/contact-us.component.ts
+++ refundme-india/src/app/components/contact-us/contact-us.component.ts
@@ -1,6 +1,4 @@
-import { Injectable, Component, OnInit, Input } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-contact-us',
@@ -8,40 +6,8 @@ import { Router } from '@angular/router';
 	styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent implements OnInit {
-
-	rForm: FormGroup;
-	post:any; 
-	message:string = '';
-	fname:string = '';
-	lname:string = '';
-	email:string = '';
-	phone:string = '';
-	jobTitle:string = '';
-	organization:string = '';
-	captcha:string = '';
-	titleAlert:string = 'This field is required.';
 	
-	constructor(private fb: FormBuilder) {
-		this.rForm = fb.group({
-			'fname' : [null, Validators.required],
-			'lname' : [null, Validators.required],
-			'email' : [null, Validators.required],
-			'phone' : [null, Validators.required],
-			'jobTitle' : [null, Validators.required],
-			'organization' : [null, Validators.required],
-			'message' : [null, Validators.compose([Validators.required, Validators.minLength(30), Validators.maxLength(500)])],
-			'captcha' : ''
-		});
-	}
-	
-	resolved(captchaResponse: string) {
-		
-        console.log(`Resolved captcha with response ${captchaResponse}:`);
-		
-    }
-	
-	saveContactUs(data, service){
-		console.log(data);
+	constructor() {
 	}
 	
 	ngOnInit() {
