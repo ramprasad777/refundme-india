b83579da7a2bce89747cdeed2f4d6efc1af1bd0f
unknown <rpaneja@gmail.com>
2018-10-30 18:00:35 +0530

    vender dashboard udpates

---
+++ src/app/components/vender-dashboard/vender-dashboard-manage-bookings/vender-dashboard-manage-bookings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vender-dashboard-manage-bookings',
+  templateUrl: './vender-dashboard-manage-bookings.component.html',
+  styleUrls: ['./vender-dashboard-manage-bookings.component.css']
+})
+export class VenderDashboardManageBookingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/vender-dashboard/vender-dashboard-reporting/vender-dashboard-reporting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vender-dashboard-reporting',
+  templateUrl: './vender-dashboard-reporting.component.html',
+  styleUrls: ['./vender-dashboard-reporting.component.css']
+})
+export class VenderDashboardReportingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
