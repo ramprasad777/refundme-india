358df958915c0670021f676224bca1d1b1666bf4
unknown <rajendra.pandey21@gmail.com>
2018-11-09 10:32:38 +0530

    my change

--- src/app/components/vender-dashboard/vender-dashboard.component.ts
+++ src/app/components/vender-dashboard/vender-dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-vender-dashboard',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vender-dashboard.component.css']
 })
 export class VenderDashboardComponent implements OnInit {
-
-  constructor() { }
+  env = environment;
+  constructor(public router: Router) { }
 
   ngOnInit() {
+  }
+
+  vendorlogout(){
+
+    localStorage.removeItem('vendorToken');
+		this.env.isVendorLoggedIn = false;
+		this.router.navigate(['/']);
+
   }
 
 }
